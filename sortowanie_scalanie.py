1536a6a6a6f464dda223911178b9e79957a75e22
emiliaaa222 <148062074+emiliaaa222@users.noreply.github.com>
2026-04-29 21:56:37 +0200

    Update sortowanie_scalanie.py

--- sortowanie_scalanie.py
+++ sortowanie_scalanie.py
@@ -1,10 +1,10 @@
-def oblicz_sume(n):
-    suma = 0
-
-    for i in range(1, n + 1):
-        suma += i
-
-    return suma
-
-wynik = oblicz_sume(5)
-print("Suma liczb od 1 do 5 wynosi:", wynik)
+def merge_sort(t):
+    if len(t) <= 1:
+        return t
+    m = len(t) // 2
+    l = merge_sort(t[:m])
+    r = merge_sort(t[m:])
+    return sorted(l + r)
+ 
+ 
+print(merge_sort([5, 2, 9, 1]))
