be519444f44b453dab30c20cae90cf6c3b8d6123
emiliaaa222 <148062074+emiliaaa222@users.noreply.github.com>
2026-05-20 18:35:10 +0200

    Update sortowanie_scalanie.py

--- sortowanie_scalanie.py
+++ sortowanie_scalanie.py
@@ -1,9 +1,29 @@
-def merge_sort(t):
+def sortowanie(t):
     if len(t) <= 1:
         return t
+
     m = len(t) // 2
-    l = merge_sort(t[:m])
-    r = merge_sort(t[m:])
-    return sorted(l + r)
- 
-print(merge_sort([5, 2, 9, 1]))
+
+    lewa = sortowanie(t[:m])
+    prawa = sortowanie(t[m:])
+
+    wynik = []
+    i = j = 0
+
+    while i < len(lewa) and j < len(prawa):
+        if lewa[i] < prawa[j]:
+            wynik.append(lewa[i])
+            i += 1
+        else:
+            wynik.append(prawa[j])
+            j += 1
+
+    wynik += lewa[i:]
+    wynik += prawa[j:]
+
+    return wynik
+
+
+tab = [4, 1, 7, 2, 9]
+
+print(sortowanie(tab))
